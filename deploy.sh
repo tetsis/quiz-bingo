d1b839e5fac1817dddc91bfacc84b2a7de99122f
Nakao Hitoshi <h.nakao@tvbu.iac-ntt.com>
2016-11-21 16:07:05 +0900

    Mod deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ DST_DIR="/var/www/html/quiz-bingo/"
 mkdir tmp
 cp -rf src/* tmp/
 cd tmp
-sed -i -e "s/^.*console\.log.*$//" *.js
+sed -i -e "s/^.*console\.log.*$//" js/*.js
 rm -rf $DST_DIR/*
 cp -rf * $DST_DIR
 chown -R apache:apache $DST_DIR
